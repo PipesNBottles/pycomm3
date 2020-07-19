af62b867ed11e0bc7a792ff1eb8d88d251ab2b5b
Khalid Al-Awady <pipes_and_bottles@protonmail.com>
2020-07-18 20:42:01 -0400

    Revert "testing to see if fixes out of bounds"
    This reverts commit 0ef5f76b8ae7eea194eea50319b95d484202125c.

--- pycomm3/slc.py
+++ pycomm3/slc.py
@@ -43,6 +43,7 @@ class SLCDriver(CipBase):
         super().__init__(path=path, legacy=True)
         self.attribs['extended forward open'] = False
 
+        self._forward_open()
     
     def __enter__(self):
         self.open()
